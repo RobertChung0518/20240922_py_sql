b2b4085f5c9185de6c6c3f6a70962307d6cd83c7
RobertChung0518 <hn85025164@yahoo.com.tw>
2024-09-29 15:44:15 +0800

    lesson2_4

---
+++ sql/lesson2_4.sql
@@ -0,0 +1,11 @@
+DROP TABLE IF EXISTS hospital;
+
+CREATE TABLE IF NOT EXISTS hospital (
+    id SERIAL PRIMARY KEY,
+    state VARCHAR(5) NOT NULL,
+	name VARCHAR(30) NOT NULL UNIQUE,
+	phone VARCHAR(15),
+	ext VARCHAR(7),
+	contact VARCHAR(7),
+	address VARCHAR(50)
+);

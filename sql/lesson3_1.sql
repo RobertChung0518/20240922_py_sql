95f271274b7bd34be92db353a5e108aa7d3af3e0
RobertChung0518 <hn85025164@yahoo.com.tw>
2024-10-06 12:00:56 +0800

    3-1

--- sql/lesson3_1.sql
+++ sql/lesson3_1.sql
@@ -47,3 +47,11 @@ WHERE name LIKE '%中';
 SELECT id as 序號,code as 代碼, name as 車站名稱, e_name as 英文名稱
 FROM taipeistation
 WHERE name LIKE '_____';
+
+SELECT id as 序號,code as 代碼, name as 車站名稱, e_name as 英文名稱
+FROM taipeistation
+WHERE id <=10;
+
+SELECT id as 序號,code as 代碼, name as 車站名稱, e_name as 英文名稱
+FROM taipeistation
+WHERE id <=50 and id >=40;

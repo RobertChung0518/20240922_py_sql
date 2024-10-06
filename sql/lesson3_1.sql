a504575aa82a66997244d654668de18aa034597d
RobertChung0518 <hn85025164@yahoo.com.tw>
2024-10-06 11:41:07 +0800

    lesson3_1

--- sql/lesson3_1.sql
+++ sql/lesson3_1.sql
@@ -12,3 +12,19 @@ FROM taipeistation;
 
 SELECT code as 代碼,name as 車站名稱,e_name as 英文名稱
 FROM taipeistation;
+
+SELECT code as 代碼,name as 車站名稱,e_name as 英文名稱
+FROM taipeistation
+WHERE code = '1001'
+
+SELECT code as 代碼,name as 車站名稱,e_name as 英文名稱
+FROM taipeistation
+WHERE code = '1001' or code = '1006'
+
+SELECT code as 代碼,name as 車站名稱,e_name as 英文名稱
+FROM taipeistation
+WHERE code IN ('1001','1006')
+
+SELECT code as 代碼,name as 車站名稱,e_name as 英文名稱
+FROM taipeistation
+WHERE code = '1001' or name = '南港'

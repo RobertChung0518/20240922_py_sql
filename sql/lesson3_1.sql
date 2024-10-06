dddd89f746c930b8a6751ae62d269c6c34cece14
RobertChung0518 <hn85025164@yahoo.com.tw>
2024-10-06 11:57:52 +0800

    3-1

--- sql/lesson3_1.sql
+++ sql/lesson3_1.sql
@@ -15,16 +15,35 @@ FROM taipeistation;
 
 SELECT code as 代碼,name as 車站名稱,e_name as 英文名稱
 FROM taipeistation
-WHERE code = '1001'
+WHERE code = '1001';
 
 SELECT code as 代碼,name as 車站名稱,e_name as 英文名稱
 FROM taipeistation
-WHERE code = '1001' or code = '1006'
+WHERE code = '1001' or code = '1006';
 
 SELECT code as 代碼,name as 車站名稱,e_name as 英文名稱
 FROM taipeistation
-WHERE code IN ('1001','1006')
+WHERE code IN ('1001','1006');
 
 SELECT code as 代碼,name as 車站名稱,e_name as 英文名稱
 FROM taipeistation
-WHERE code = '1001' or name = '南港'
+WHERE code = '1001' or name = '南港';
+
+SELECT id as 序號,code as 代碼, name as 車站名稱, e_name as 英文名稱
+FROM taipeistation;
+
+SELECT id as 序號,code as 代碼, name as 車站名稱, e_name as 英文名稱
+FROM taipeistation
+WHERE name LIKE '台_';
+
+SELECT id as 序號,code as 代碼, name as 車站名稱, e_name as 英文名稱
+FROM taipeistation
+WHERE name LIKE '台%';
+
+SELECT id as 序號,code as 代碼, name as 車站名稱, e_name as 英文名稱
+FROM taipeistation
+WHERE name LIKE '%中';
+
+SELECT id as 序號,code as 代碼, name as 車站名稱, e_name as 英文名稱
+FROM taipeistation
+WHERE name LIKE '_____';

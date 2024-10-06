ef22553b76040bce5258732006bb8367130152d4
RobertChung0518 <hn85025164@yahoo.com.tw>
2024-10-06 11:23:21 +0800

    lesson3_1

---
+++ sql/lesson3_1.sql
@@ -0,0 +1,14 @@
+SELECT code,name
+FROM taipeistation;
+
+SELECT *
+FROM taipeistation;
+
+SELECT code as 代碼
+FROM taipeistation;
+
+SELECT code as 代碼,name as 車站名稱
+FROM taipeistation;
+
+SELECT code as 代碼,name as 車站名稱,e_name as 英文名稱
+FROM taipeistation;

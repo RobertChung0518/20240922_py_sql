af10184ce0746c375f1a4518d95e0bd35819f48a
RobertChung0518 <hn85025164@yahoo.com.tw>
2024-10-06 14:29:39 +0800

    3-2

--- sql/lesson3_1.sql
+++ sql/lesson3_1.sql
@@ -55,3 +55,39 @@ WHERE id <=10;
 SELECT id as 序號,code as 代碼, name as 車站名稱, e_name as 英文名稱
 FROM taipeistation
 WHERE id <=50 and id >=40;
+
+SELECT id as 序號,code as 代碼, name as 車站名稱, e_name as 英文名稱
+FROM taipeistation
+WHERE id <10 or id >20;
+
+SELECT id as 序號,code as 代碼, name as 車站名稱, e_name as 英文名稱
+FROM taipeistation
+WHERE id BETWEEN 10 AND 20;
+
+SELECT id as 序號_id,code as 代碼_code, name as 車站名稱_name, e_name as 英文名稱_e_name
+FROM taipeistation
+WHERE id NOT BETWEEN 10 AND 20;
+
+SELECT id as 序號_id,code as 代碼_code, name as 車站名稱_name, e_name as 英文名稱_e_name
+FROM taipeistation
+ORDER BY id;
+
+SELECT id as 序號_id,code as 代碼_code, name as 車站名稱_name, e_name as 英文名稱_e_name
+FROM taipeistation
+ORDER BY id DESC;
+
+SELECT id as 序號_id,code as 代碼_code, name as 車站名稱_name, e_name as 英文名稱_e_name
+FROM taipeistation
+ORDER BY code;
+
+SELECT id as 序號_id,code as 代碼_code, name as 車站名稱_name, e_name as 英文名稱_e_name
+FROM taipeistation
+ORDER BY code DESC;
+
+SELECT id as 序號_id,code as 代碼_code, name as 車站名稱_name, e_name as 英文名稱_e_name
+FROM taipeistation
+ORDER BY e_name;
+
+SELECT id as 序號_id,code as 代碼_code, name as 車站名稱_name, e_name as 英文名稱_e_name
+FROM taipeistation
+ORDER BY e_name DESC;

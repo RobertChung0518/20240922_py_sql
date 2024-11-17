ce1291251d166a95a8b41d48d83d1c0a6b5bbe97
RobertChung0518 <hn85025164@yahoo.com.tw>
2024-11-17 12:00:09 +0800

    test pprint and rich

--- lesson9/tools/__datasource__.py
+++ lesson9/tools/__datasource__.py
@@ -9,3 +9,9 @@ import time
 #自訂的function
 def get_data():
     time.sleep(2)
+
+import requests
+#TPE ubike data
+url = 'https://tcgbusfs.blob.core.windows.net/dotapp/youbike/v2/youbike_immediate.json'
+reponse = requests.get(url)
+type(reponse)
